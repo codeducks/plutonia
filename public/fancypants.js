662709159fb8fd90ce12fe063b46abfd33716fcd
faultydev <faultydev@gmail.com>
2021-03-14 00:30:35 +0100

    init

---
+++ public/fancypants.js
@@ -0,0 +1,9 @@
+function stackoverflow() {
+    var txt = document.getElementById("search").value
+    window.location.href = "https://google.com/search?q=" + escape(txt)
+}
+
+function google(){
+    var txt = document.getElementById("search").value
+    window.location.href = "https://google.com/search?q=" + escape(txt)
+}
