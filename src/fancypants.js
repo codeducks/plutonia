d2c935685c3eb417054d9549a36118d96378dd2e
faultydev <faultydev@gmail.com>
2021-03-14 02:15:13 +0100

    update..

--- src/fancypants.js
+++ src/fancypants.js
@@ -1,9 +1,9 @@
 function stackoverflow() {
-    var txt = document.getElementById("search").value
-    window.location.href = "https://stackoverflow.com/search?q=" + escape(txt)
+    var txt = document.getElementById("search").value;
+    window.location.href = "https://stackoverflow.com/search?q=" + escape(txt);
 }
 
 function google(){
-    var txt = document.getElementById("search").value
-    window.location.href = "https://google.com/search?q=" + escape(txt)
+    var txt = document.getElementById("search").value;
+    window.location.href = "https://google.com/search?q=" + escape(txt);
 }
